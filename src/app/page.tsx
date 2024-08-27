52a1d7c92082be5f3055cb84866298d83e9e114b
Solene <solene.blanc@gmail.com>
2024-08-27 13:19:50 +0200

    ajout responsive avec une media querie tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MeteoCard from "@/components/MeteoCard";
 export default function Home() {
   console.log("execution home");
   return (
-    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-2">
       <MeteoCard city="Paris" />
       <MeteoCard city="Montpellier" />
     </div>
